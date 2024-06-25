6110cb57cc837d5493fda5161d4248454c1d0a38
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-06-25 13:59:40 +0900

    final setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Wikid</title>
+        <link
+          rel="preload"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+          as="style"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
