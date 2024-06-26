f684d27057fd80cad25325409a6faafae7ac7e0b
hyunow <stophyun_13@naver.com>
2024-06-26 21:30:49 +0900

    refactor: Header Component 중복된 부분 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,24 +8,7 @@ import ProfileIcon from "@icons/ic_profile.svg";
 
 const HeaderLoggedOut: React.FC = () => {
   return (
-    <div className="shadow-m flex h-[80px] w-full items-center justify-between bg-[var(--color-white)] pl-[20px] pr-[20px] shadow-md">
-      <div className="flex items-center gap-[40px]">
-        <Link href="/">
-          <Image src={Logo} alt="로고이미지" className="h-[30px] w-[107px]" />
-        </Link>
-        <Link
-          href="/wikilist"
-          className="text-[14px] font-normal text-[var(--color-gray800)]"
-        >
-          위키목록
-        </Link>
-        <Link
-          href="/boards"
-          className="text-[14px] font-normal text-[var(--color-gray800)]"
-        >
-          자유게시판
-        </Link>
-      </div>
+    <div>
       <Link
         href="/login"
         className="text-[14px] font-normal text-[var(--color-gray600)]"
@@ -37,6 +20,19 @@ const HeaderLoggedOut: React.FC = () => {
 };
 
 const HeaderLoggedIn: React.FC = () => {
+  return (
+    <div className="flex items-center gap-[24px]">
+      <IconButton src={AlarmIcon} alt="알람 아이콘" />
+      <IconButton src={ProfileIcon} alt="프로필 아이콘" />
+    </div>
+  );
+};
+
+interface HeaderProps {
+  isLoggedIn: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
   return (
     <div className="shadow-m flex h-[80px] w-full items-center justify-between bg-[var(--color-white)] pl-[20px] pr-[20px] shadow-md">
       <div className="flex items-center gap-[40px]">
@@ -56,20 +52,9 @@ const HeaderLoggedIn: React.FC = () => {
           자유게시판
         </Link>
       </div>
-      <div className="flex items-center gap-[24px]">
-        <IconButton src={AlarmIcon} alt="알람 아이콘" />
-        <IconButton src={ProfileIcon} alt="프로필 아이콘" />
-      </div>
+      {isLoggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
     </div>
   );
-};
-
-interface HeaderProps {
-  isLoggedIn: boolean;
-}
-
-const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
-  return <>{isLoggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}</>;
 };
 
 export default Header;
