41babb30bac2c6cb16154db0dba7b7fcae0afd59
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-06-27 13:41:54 +0900

    test: onChange 연결되는지 확인, wikipage작업 들가면됨

--- src/pages/addboard.tsx
+++ src/pages/addboard.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import TextEditor from "@components/TextEditor";
 
 function ArticleEditPage() {
+  const [content, setContent] = useState("");
+
+  const handleChange = (value: string) => {
+    setContent(value);
+  };
+
   return (
     <div>
-      <TextEditor type="article" />
+      <TextEditor type="article" content={content} onChange={handleChange} />
     </div>
   );
 }
