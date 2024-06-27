cf1eb5e73f6317e278312b1e416f514cf0249927
hyunow <stophyun_13@naver.com>
2024-06-27 13:27:32 +0900

    fix: Header 프로필 이미지 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import IconButton from "./IconButton";
 import Logo from "@images/image_logo.png";
 import AlarmIcon from "@icons/ic_alarm.svg";
-import ProfileIcon from "@icons/ic_profile.svg";
+import DefaultProfileIcon from "@icons/ic_profile.svg";
 
 const HeaderLoggedOut: React.FC = () => {
   return (
@@ -19,20 +19,33 @@ const HeaderLoggedOut: React.FC = () => {
   );
 };
 
-const HeaderLoggedIn: React.FC = () => {
+interface HeaderLoggedInProps {
+  profileIconSrc?: string | StaticImageData;
+}
+
+const HeaderLoggedIn: React.FC<HeaderLoggedInProps> = ({ profileIconSrc }) => {
   return (
     <div className="flex items-center gap-[24px]">
-      <IconButton src={AlarmIcon} alt="알람 아이콘" />
-      <IconButton src={ProfileIcon} alt="프로필 아이콘" />
+      <IconButton
+        src={AlarmIcon}
+        alt="알람 아이콘"
+        className="h-[32px] w-[32px]"
+      />
+      <IconButton
+        src={profileIconSrc || DefaultProfileIcon}
+        alt="프로필 아이콘"
+        className="h-[32px] w-[32px] rounded-full"
+      />
     </div>
   );
 };
 
 interface HeaderProps {
   isLoggedIn: boolean;
+  profileIconSrc?: string | StaticImageData;
 }
 
-const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
+const Header: React.FC<HeaderProps> = ({ isLoggedIn, profileIconSrc }) => {
   return (
     <div className="shadow-m flex h-[80px] w-full items-center justify-between bg-[var(--color-white)] pl-[20px] pr-[20px] shadow-md">
       <div className="flex items-center gap-[40px]">
@@ -52,7 +65,11 @@ const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
           자유게시판
         </Link>
       </div>
-      {isLoggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+      {isLoggedIn ? (
+        <HeaderLoggedIn profileIconSrc={profileIconSrc} />
+      ) : (
+        <HeaderLoggedOut />
+      )}
     </div>
   );
 };
