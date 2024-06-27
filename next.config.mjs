d5d57feac7789e406476bc0ef0075fa009ad2214
song-ari <sari0708@ajou.ac.kr>
2024-06-28 00:57:09 +0900

    feat: CORS 에러 해결을 위한 프록시 서버 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // 프록시할 경로 패턴
+        destination: "https://wikied-api.vercel.app/:path*", // 실제 API 서버 주소
+      },
+    ];
+  },
 };
 
 export default nextConfig;
