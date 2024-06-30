78db64e48d44ad00fc728517ee4341f5845eecb9
kyj <dbwjd338@gmail.com>
2024-07-01 02:56:31 +0900

    feat: 링크 복사 버튼 추가

--- src/components/wikilist/UserWikiCard.tsx
+++ src/components/wikilist/UserWikiCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+// import LinkCopyButton from "@components/LinkCopyButton";
 import { Profile } from "@lib/types/Profile";
 import profileIcon from "@icons/ic_profile.svg";
 
@@ -12,21 +13,24 @@ const UserWikiCard = ({ profile }: UserWikiCardProps) => {
     <div className="mb-[24px] rounded-[10px] shadow-[0_4px_20px_0_rgba(0,0,0,0.08)]">
       <Link
         href={`/wiki/${profile.code}`}
-        className="flex h-[140px] gap-[32px] p-[25px_35px]"
+        className="flex h-[140px] items-end justify-between gap-[20px] p-[25px_35px]"
       >
-        <Image
-          src={profile.image || profileIcon}
-          alt={profile.name}
-          height={85}
-          width={85}
-        />
-        <div>
-          <div className="mb-[15px] text-[24px] font-semibold">
-            {profile.name}
+        <div className="flex gap-[32px]">
+          <Image
+            src={profile.image || profileIcon}
+            alt={profile.name}
+            height={85}
+            width={85}
+          />
+          <div>
+            <div className="mb-[15px] text-[24px] font-semibold">
+              {profile.name}
+            </div>
+            <div className="text-gray400">{profile.city}</div>
+            <div className="text-gray400">{profile.job}</div>
           </div>
-          <div className="text-gray400">{profile.city}</div>
-          <div className="text-gray400">{profile.job}</div>
         </div>
+        {/* <LinkCopyButton link="https://www.wikied.kr/wikicode" /> */}
       </Link>
     </div>
   );
