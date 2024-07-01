e8584aa0a20ed629e962fe976993b934e6d77b21
kyj <dbwjd338@gmail.com>
2024-07-02 00:56:50 +0900

    fix: 페이지네이션 기능 수정

--- src/components/wikilist/WikiListBox.tsx
+++ src/components/wikilist/WikiListBox.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import PaginationBar from "@components/PaginationBar";
 import UserWikiList from "./UserWikiList";
+import usePagination from "@hooks/usePagination";
 import { getProfileList } from "@lib/api/profileApi";
 import { Profile } from "@lib/types/Profile";
 
@@ -8,8 +9,13 @@ const PAGE_SIZE = 3;
 
 const WikiListBox = () => {
   const [items, setItems] = useState<Profile[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPage, setTotalPage] = useState<number>(0);
+
+  const { currentPage, handleGoPage, handlePrevPage, handleNextPage } =
+    usePagination({
+      initialPage: 1,
+      totalPage,
+    });
 
   const handleLoad = async (options: { page: number; pageSize: number }) => {
     try {
@@ -28,7 +34,13 @@ const WikiListBox = () => {
   return (
     <>
       <UserWikiList items={items} />
-      <PaginationBar currentPage={currentPage} totalPage={totalPage} />
+      <PaginationBar
+        currentPage={currentPage}
+        totalPage={totalPage}
+        handleGoPage={handleGoPage}
+        handlePrevPage={handlePrevPage}
+        handleNextPage={handleNextPage}
+      />
     </>
   );
 };
