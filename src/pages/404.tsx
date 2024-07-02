0de01f915027c553090412ab59be3bc43977710e
hyunow <stophyun_13@naver.com>
2024-07-03 03:08:32 +0900

    style: 404 페이지 스타일 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,27 @@
+import Animation from "react-lottie-player";
 import LinkButton from "@components/LinkButton";
+import AnimationJson from "../../public/Animation404.json";
 
 export default function NotFound() {
   return (
-    <div className="mt-[350px] flex flex-col items-center gap-[50px]">
-      <div className="flex flex-col items-center gap-[20px]">
-        <p className="text-[30px] font-normal">찾을 수 없는 페이지입니다. </p>
-      </div>
-      <LinkButton
-        text="홈으로 이동"
-        link="/"
-        color="green"
-        className="h-[50px] w-[200px] text-[25px] font-normal"
+    <div className="mt-[90px] flex flex-col items-center">
+      <Animation
+        loop
+        animationData={AnimationJson}
+        play
+        style={{ width: 800, height: 500 }}
       />
+      <div className="flex flex-col items-center gap-[20px]">
+        <p className="text-[20px] font-normal text-green200">
+          페이지를 찾을 수 없습니다
+        </p>
+        <LinkButton
+          text="홈으로 이동"
+          link="/"
+          color="green"
+          className="h-[45px] w-[150px] text-[20px] font-normal"
+        />
+      </div>
     </div>
   );
 }
