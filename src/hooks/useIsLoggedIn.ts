79f1f57b46fccc57f58ce54ec43b0b4b08830147
hyunow <stophyun_13@naver.com>
2024-07-04 22:26:36 +0900

    refator: 로그인, 로그아웃 로직 분리

--- src/hooks/useIsLoggedIn.ts
+++ src/hooks/useIsLoggedIn.ts
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
-const useAuth = () => {
+// 이거 이름 useAuth에서 useIsLoggedIn 으로 바꿀게요
+// AuthContext 안에 useAuth가 존재해요
+const useIsLoggedIn = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -16,4 +18,4 @@ const useAuth = () => {
   return { isLoggedIn };
 };
 
-export default useAuth;
+export default useIsLoggedIn;
