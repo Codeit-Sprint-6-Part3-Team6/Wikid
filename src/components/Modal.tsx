f79d0763a184f0d031623c47592732e4b616cef6
MJY1993 <mjylove26@gmail.com>
2024-07-05 02:01:32 +0900

    refactor: 모달 css 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,9 +13,9 @@ export default function Modal({ isOpen, handleIsOpen, children }: ModalProps) {
         <div className="fixed inset-0 flex h-[100vh] w-full items-center justify-center">
           <div
             onClick={handleIsOpen}
-            className="absolute inset-0 bg-[#474D664D]"
+            className="absolute inset-[0px] bg-[#474D664D]"
           ></div>
-          <div className="relative inset-0 z-50 m-[0_auto] flex w-[335px] flex-col rounded-lg bg-white p-5 text-black shadow-2xl backdrop-blur-md sm:min-w-[395px]">
+          <div className="z-50 flex w-[335px] flex-col rounded-lg bg-white p-5 text-black shadow-2xl sm:min-w-[395px]">
             <button className="mb-2 place-self-end text-[#8F95B2]">
               <X onClick={handleIsOpen} />
             </button>
