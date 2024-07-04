33e673d5ad60c42cfcccfce48cb84a58e3944c88
song-ari <sari0708@ajou.ac.kr>
2024-07-04 10:45:34 +0900

    feat: 댓글 프로필 이미지 스타일 적용

--- src/components/article/CommentCard.tsx
+++ src/components/article/CommentCard.tsx
@@ -54,7 +54,13 @@ const CommentCard = ({
   return (
     <CardContainer className="mb-[16px] items-start py-[16px] md:py-[22px] lg:mb-[24px]">
       <div className="flex flex-1 grow items-start gap-[15px] md:gap-[20px]">
-        <Image src={imageSrc} alt="프로필 이미지" width={50} />
+        <Image
+          src={imageSrc}
+          alt="프로필 이미지"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
         <div className="flex w-full flex-col">
           <span className="text-[16px] font-semibold md:mb-[6px] md:text-[18px]">
             {comment.writer.name}
