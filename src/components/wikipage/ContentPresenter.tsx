6657b33b136e371f700366595c041cfaea3be2fa
임희서 <imagic@imhuiseoui-MacBookAir.local>
2024-07-05 12:51:42 +0900

    style: 빈 위키 스타일 추가

--- src/components/wikipage/ContentPresenter.tsx
+++ src/components/wikipage/ContentPresenter.tsx
@@ -10,11 +10,16 @@ function ContentPresenter({ content }: { content: string }) {
       }}
     />
   ) : (
-    <div>
+    <div className="flex h-48 flex-col items-center justify-center gap-[20px] rounded-[10px] bg-gray50 text-center text-[16px] leading-[26px] text-gray400">
       아직 작성된 내용이 없네요.
       <br />
       위키에 참여해 보세요!
-      <Button type="button" text="시작하기" color="green" />
+      <Button
+        type="button"
+        text="시작하기"
+        color="green"
+        className="px-5 py-2"
+      />
     </div>
   );
 }
