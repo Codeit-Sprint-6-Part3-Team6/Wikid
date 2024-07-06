426a77818a3797fabe038fd4ad16e5863b40833e
song-ari <sari0708@ajou.ac.kr>
2024-07-06 12:39:57 +0900

    refactor: 이상한 파일들 삭제

--- src/lib/inputCounter 2.ts
+++
@@ -1,9 +0,0 @@
-export const inputCounter = (
-  e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
-  setInputCount: (count: number) => void,
-  setContent: (content: string) => void,
-) => {
-  const newValue = e.target.value;
-  setInputCount(newValue.length);
-  setContent(newValue);
-};
