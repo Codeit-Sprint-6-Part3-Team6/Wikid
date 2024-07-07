d05d4e834eeb4eb9b3baf11a04bffb0826e6b4c8
song-ari <sari0708@ajou.ac.kr>
2024-07-07 13:24:48 +0900

    feat: 로고 및 타이틀 변경

--- src/components/SmallCat.tsx
+++ src/components/SmallCat.tsx
@@ -4,7 +4,7 @@ import loadingCat from "@images/image_loading.gif";
 export default function SmallCat() {
   return (
     <div className="absolute inset-0 flex items-center">
-      <Image alt="loading-cat" src={loadingCat} className="-rotate-[15deg]" />;
+      <Image alt="loading-cat" src={loadingCat} className="-rotate-[15deg]" />
     </div>
   );
 }
