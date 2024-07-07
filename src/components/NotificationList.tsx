454031965c8e4ea542bf7d8c9652f987fdfdda19
MJY1993 <mjylove26@gmail.com>
2024-07-07 10:41:21 +0900

    feat: 알람이 없을 경우 고양이를 렌더링..

--- src/components/NotificationList.tsx
+++ src/components/NotificationList.tsx
@@ -1,5 +1,6 @@
 import { X } from "lucide-react";
 import Link from "next/link";
+import SmallCat from "./SmallCat";
 import useOutsideClick from "@hooks/useOutsideClick";
 import useUserInfo from "@hooks/useUserInfo";
 import { elapsedTimeConverter } from "@lib/elapsedTimeConverter";
@@ -31,25 +32,29 @@ export default function NotificationList({
         <div
           ref={notificationRef}
           id="outer-most-div"
-          className="max-h-[285px] w-[280px] rounded-xl bg-[#CED8D5] px-[20px] py-[24px] text-[11px] shadow-xl sm:w-[310px] sm:text-[13px] lg:w-[368px] lg:text-[14px]"
+          className={` ${notificationList.length ? "max-h-[285px]" : `h-[285px]`} w-[280px] rounded-xl bg-[#CED8D5] px-[20px] py-[24px] text-[11px] shadow-xl sm:w-[310px] sm:text-[13px] lg:w-[368px] lg:text-[14px]`}
         >
           <div className="flex w-full items-center justify-between text-[#1B1B1B]">
             <p className="text-[16px] font-[500] sm:text-[20px]">
-              알림 {totalCount}개
+              {notificationList.length ? `알림 ${totalCount}개` : `로딩캣 태비`}
             </p>
             <X onClick={handleIsOpen} className="cursor-pointer" />
           </div>
-          <div className="max-h-[230px] overflow-y-scroll">
+          <div
+            className={`relative ${notificationList.length ? "max-h-[230px]" : `h-[230px]`} overflow-y-scroll`}
+          >
             <div className="my-[20px] flex h-full flex-col items-center gap-3">
-              {notificationList
-                ? notificationList.map((item) => (
-                    <NotificationItem
-                      key={item.id}
-                      data={item}
-                      onClick={handleDeleteClick}
-                    />
-                  ))
-                : null}
+              {notificationList.length ? (
+                notificationList.map((item) => (
+                  <NotificationItem
+                    key={item.id}
+                    data={item}
+                    onClick={handleDeleteClick}
+                  />
+                ))
+              ) : (
+                <SmallCat />
+              )}
             </div>
           </div>
         </div>
